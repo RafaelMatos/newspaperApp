73b1ae4ad8809a7d2af59e3521de4080d09d910f
RafaelMatos <rafael-matos@outlook.com>
2019-10-15 11:36:42 -0300

    Initial commit - CRUD Articles

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/mal/Documentos/Codes/Django/Django for Beginners/Chapter 8/news/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
