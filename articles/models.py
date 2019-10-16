e6faecdd793f1d9c9f4ddb4285725e61ce9b2f42
RafaelMatos <rafael-matos@outlook.com>
2019-10-16 09:34:04 -0300

    Article comment section added

--- articles/models.py
+++ articles/models.py
@@ -10,6 +10,8 @@ class Article(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+    link = models.CharField(max_length=255, null=True)
+    share = models.BooleanField(default=False)
     author = models.ForeignKey(
         get_user_model(),
         on_delete=models.CASCADE
@@ -20,3 +22,14 @@ class Article(models.Model):
     
     def get_absolute_url(self):
         return reverse('article_detail',args=[str(self.id)])
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
+    comment = models.CharField(max_length=140)
+    author = models.ForeignKey(get_user_model(),on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.comment
+    
+    def ger_absolute_url(self):
+        return reverse('article_list')
